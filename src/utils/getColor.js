0e939082d0648221873c381ad20719c91adf198f
SofiDev <angiemeow89@gmail.com>
2025-08-08 01:45:12 -0600

    🚧 Hero section in progress
    Co-authored-by: Stron - Jose Luis B. Leyva <elstron@users.noreply.github.com>
    Co-authored-by: Ricardo Vega G <contact@rickytodev.xyz>

--- src/utils/getColor.js
+++ src/utils/getColor.js
@@ -1,12 +1,12 @@
 const titleVariants = {
-    default: "bg-gray-500 text-white hover:bg-gray-600 focus:ring-gray-500 p-3  text-center rounded-md font-bold cursor-pointer max-w-[max-content]", // Default
-    primary: "bg-violet-900 text-violet-400 hover:bg-violet-600 hover:text-white focus:ring-blue-500 p-3  text-center rounded-md font-bold cursor-pointer max-w-[max-content]", // Primary
-    secondary: "bg-[#3e3947] text-white hover:bg-[#3e3947cd] focus:ring-purple-500 p-3  text-center rounded-md font-bold cursor-pointer max-w-[max-content]", // Secondary
-    accent: "bg-[#2d3e6b] text-[#3b82f6] hover:bg-[#2d3e6bda] focus:ring-indigo-500 p-3  text-center rounded-md font-bold cursor-pointer max-w-[max-content]", // Accent
-    info: "bg-[#1d4e62] textwhite hover:bg-[#245e75] focus:ring-cyan-500 p-3  text-center rounded-md font-bold cursor-pointer max-w-[max-content]", // Info
-    success: "bg-[#1b5439] text-[#02ca4b] hover:bg-[#216545] focus:ring-green-500 p-3  text-center rounded-md font-bold cursor-pointer max-w-[max-content]", // Success
-    warning: "bg-yellow-800 text-yellow-200 hover:bg-yellow-600 focus:ring-yellow-500 p-3  text-center rounded-md font-bold cursor-pointer max-w-[max-content]", // Warning
-    error: "bg-red-500 text-white hover:bg-red-600 focus:ring-red-500 p-3  text-center rounded-md font-bold cursor-pointer max-w-[max-content]", // Error
+    default: "bg-gray-500 text-white hover:bg-gray-600 focus:ring-gray-500 cursor-pointer max-w-[max-content]", // Default
+    primary: "bg-violet-900 text-violet-400 hover:bg-violet-600 hover:text-white focus:ring-blue-500  ",
+    secondary: "bg-[#3e3947] text-white hover:bg-[#3e3947cd] focus:ring-purple-500  ", // Secondary
+    accent: "bg-[#2d3e6b] text-[#3b82f6] hover:bg-[#2d3e6bda] focus:ring-indigo-500  ", // Accent
+    info: "bg-[#1d4e62] textwhite hover:bg-[#245e75] focus:ring-cyan-500  ", // Info
+    success: "bg-[#1b5439] text-[#02ca4b] hover:bg-[#216545] focus:ring-green-500  ", // Success
+    warning: "bg-yellow-800 text-yellow-200 hover:bg-yellow-600 focus:ring-yellow-500  ", // Warning
+    error: "bg-red-500 text-white hover:bg-red-600 focus:ring-red-500  ", // Error
   };
 
 
